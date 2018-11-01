1a5a937a76dc3c55a8867d1ea5e7c59d4ca5fc50
Ravi Sheth <rrjsheth@gmail.com>
2018-11-01 12:08:40 -0700

    added vallartas pic and added code that will not allow you to click anymore after winner is decided

--- tournament.js
+++ tournament.js
@@ -8,8 +8,8 @@ var imageNames;
 var winnerShown;
 $(document).ready(function() {
 	winnerShown = false;
-	topCardList = ["Olive Garden", "Vallartas", "Olive Garden"];
-	bottomCardList = ["Chipotle","In-N-Out","Vallartas"]
+	topCardList = ["Olive Garden", "Vallartas", "Vallartas"];
+	bottomCardList = ["Chipotle","In-N-Out","Olive Garden"]
 	address = []
 	address["Chipotle"]="Costa Verde Center,San Diego, CA 92109"
 	address["In-N-Out"]="2910 Damon Ave, San Diego, CA 92109"
@@ -19,7 +19,7 @@ $(document).ready(function() {
 	imageNames = []
 	imageNames["Chipotle"]="chipotle.jpg"
 	imageNames["In-N-Out"]="in-n-out.png"
-	imageNames["Vallartas"]="#"
+	imageNames["Vallartas"]="vallartas.png"
 	imageNames["Olive Garden"]="olive-garden.jpg"
 
 	imagePath = "./img/sample/"
@@ -44,28 +44,36 @@ $("#bottomButton").click(toggleClass);
 
 function toggleClass(event) {
 	//alert("in method");
-	indexTop++;
-	indexBottom++;
-	if(indexTop >= 3 || !winnerShown){
+	if(indexTop >= 2 && !winnerShown){
 		//alert("in winner");
 		winnerShown = true;
 		showWinner();
-
+		//$("#topButton").prop("disabled",true);
+		$("#topButton").attr('disabled','disabled');
+		$("#topButton").children().attr("disabled","disabled");
+		$("#topButton").off("click");
+		return;
 	}
+	else{
+		indexTop++;
+		indexBottom++;
 
-	$('#topCard').find('.cardTitle').text(topCardList[indexBottom]);
-	$('#bottomCard').find('.cardTitle').text(bottomCardList[indexTop]);
+		$('#topCard').find('.cardTitle').text(topCardList[indexBottom]);
+		$('#bottomCard').find('.cardTitle').text(bottomCardList[indexTop]);
 
-	$('#topAddress').text(address[topCardList[indexTop]]);
-	$('#bottomAddress').text(address[bottomCardList[indexBottom]]);
+		$('#topAddress').text(address[topCardList[indexTop]]);
+		$('#bottomAddress').text(address[bottomCardList[indexBottom]]);
 
-	alert(bottomCardList[indexBottom]);
-	$('#topCard').find('.mdl-card__title').css("background","url('"+imagePath+imageNames[topCardList[indexTop]]+"') center");
-	$('#bottomCard').find('.mdl-card__title').css("background","url('"+imagePath+imageNames[bottomCardList[indexBottom]]+"') center");
+		//alert(bottomCardList[indexBottom]);
+		$('#topCard').find('.mdl-card__title').css("background","url('"+imagePath+imageNames[topCardList[indexTop]]+"') center");
+		$('#bottomCard').find('.mdl-card__title').css("background","url('"+imagePath+imageNames[bottomCardList[indexBottom]]+"') center");
+	}
+	
 }
 
 function showWinner() {
 	//alert("in show winner");
 	$('.page-content').prepend("<h2>Winner</h2>");
+	$('#topCard').find('.mdl-card__title').css("background","url('"+imagePath+imageNames[topCardList[indexTop]]+"') center");
 	$('#bottomHalf').html('<div></div>');
 }
