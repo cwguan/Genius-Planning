3f12c0e0ff20929775e7367b91783fa5d1acaefa
Ravi Sheth <rrjsheth@gmail.com>
2018-11-01 00:41:55 -0700

    changed text to be consistent with Seth and button text to choose; centered elements and tried to show images in the correct spot

--- tournament.js
+++ tournament.js
@@ -3,9 +3,10 @@ var bottomCardList;
 var address;
 var indexTop;
 var indexBottom;
+var imagePath;
+var imageNames;
 
 $(document).ready(function() {
-	alert("in page");
 	topCardList = ["Olive Garden", "Vallartas", "Olive Garden"];
 	bottomCardList = ["Chipotle","In-N-Out","Vallartas"]
 	address = []
@@ -13,13 +14,27 @@ $(document).ready(function() {
 	address["In-N-Out"]="2910 Damon Ave, San Diego, CA 92109"
 	address["Vallartas"]="4277 Genesee Ave, San Diego, CA 92117"
 	address["Olive Garden"]="3215 Sports Arena Blvd, San Diego, CA 92110"
+
+	imageNames = []
+	imageNames["Chipotle"]="chipotle.jpg"
+	imageNames["In-N-Out"]="in-n-out.png"
+	imageNames["Vallartas"]="#"
+	imageNames["Olive Garden"]="olive-garden.jpg"
+
+	imagePath = "./img/sample/"
 	indexTop = 0;
 	indexBottom = 0;
 
-	$('#bottomCard').find('.cardTitle').text(topCardList[indexTop]);
-	$('#topCard').find('.cardTitle').text(bottomCardList[indexBottom]);
+	$('#topCard').find('.cardTitle').text(topCardList[indexBottom]);
+	$('#bottomCard').find('.cardTitle').text(bottomCardList[indexTop]);
+
 	$('#topAddress').text(address[topCardList[indexTop]]);
 	$('#bottomAddress').text(address[bottomCardList[indexBottom]]);
+
+	$('#topCard').find('.mdl-card__title').css("background","url('"+imagePath+imageNames[topCardList[indexTop]]+"') center");
+	$('#bottomCard').find('.mdl-card__title').css("background","url('"+imagePath+imageNames[bottomCardList[indexBottom]]+"') center");
+	//$('#topCard.mdl-card__title').css("background-size",50+"px " + 70 + "%");
+	//$('#topCard.mdl-card__title').css("background-repeat","no-repeat");
 })
 
 $("#topButton").click(toggleClass);
@@ -35,10 +50,15 @@ function toggleClass(event) {
 		showWinner();
 	}
 
-	$('#bottomCard').find('.cardTitle').text(topCardList[indexTop]);
-	$('#topCard').find('.cardTitle').text(bottomCardList[indexBottom]);
+	$('#topCard').find('.cardTitle').text(topCardList[indexBottom]);
+	$('#bottomCard').find('.cardTitle').text(bottomCardList[indexTop]);
+
 	$('#topAddress').text(address[topCardList[indexTop]]);
 	$('#bottomAddress').text(address[bottomCardList[indexBottom]]);
+
+	alert(bottomCardList[indexBottom]);
+	$('#topCard').find('.mdl-card__title').css("background","url('"+imagePath+imageNames[topCardList[indexTop]]+"') center");
+	$('#bottomCard').find('.mdl-card__title').css("background","url('"+imagePath+imageNames[bottomCardList[indexBottom]]+"') center");
 }
 
 function showWinner() {
