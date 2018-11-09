c4e73cfa8c66c8a0cf2ff48c56e7c63471d03dab
Ravi Sheth <rrjsheth@gmail.com>
2018-11-08 22:03:59 -0800

    initial code before pull

--- tournament.js
+++ tournament.js
@@ -1,11 +1,13 @@
 var winnerShown;
 var totalList;
-
+var index;
+var startlength;
 $(document).ready(function() {
 
 	totalList =  JSON.parse(sessionStorage.getItem("selectedRestaurants"));
 	winnerShown = false;
-
+	index = 1;
+	startlength = totalList.length;
 	$('#topCard').find('.cardTitle').text(totalList[0].name);
 	$('#bottomCard').find('.cardTitle').text(totalList[1].name);
 
@@ -15,17 +17,22 @@ $(document).ready(function() {
 	$('#topCard').find('.mdl-card__title').css("background","url('"+totalList[0].image_url+"') center");
 	$('#bottomCard').find('.mdl-card__title').css("background","url('"+totalList[1].image_url+"') center");
 
+	//document.querySelector('#p1').MaterialProgress.setProgress(0);
 	//$('#topCard.mdl-card__title').css("background-size",50+"px " + 70 + "%");
 	//$('#topCard.mdl-card__title').css("background-repeat","no-repeat");
 })
 
 $("#topButton").click(topButtonClick);
 $("#bottomButton").click(bottomButtonClick);
+document.querySelector('#p1').addEventListener('mdl-componentupgraded', function() {
+    this.MaterialProgress.setProgress(44);
+  });
 
 function topButtonClick(event){
 	totalList.push(totalList.shift()); //add the first element in html
 	totalList.shift();
-
+	index++;
+	document.querySelector('#p1').MaterialProgress.setProgress(((index*100.0)/startlength));
 	if(totalList.length <= 1){
 		winnerShown = true;
 		showWinner(totalList[0]);
@@ -45,7 +52,9 @@ function bottomButtonClick(event) {
 
 	totalList.shift();
 	totalList.push(totalList.shift()); //add the first element in html
-
+	index++;
+	//document.querySelector
+	document.querySelector('#p1').MaterialProgress.setProgress(((index*100.0)/startlength));
 	if(totalList.length <= 1){
 		winnerShown = true;
 		showWinner(totalList[0]);
